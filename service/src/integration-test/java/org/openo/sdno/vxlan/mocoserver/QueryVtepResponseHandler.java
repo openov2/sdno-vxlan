ef6012eececa123cf618c15b89b4fed8f4b63d2e
openo-lihua <uranus.lihua@huawei.com>
2016-09-07 22:08:20 +0800

    add IT codes
    Change-Id: Ib89e53b8fe025b78f5baef83c3d117898302a53f
    Signed-off-by: openo-lihua <uranus.lihua@huawei.com>

---
+++ service/src/integration-test/java/org/openo/sdno/vxlan/mocoserver/QueryVtepResponseHandler.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016, Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdno.vxlan.mocoserver;
+
+import org.codehaus.jackson.type.TypeReference;
+import org.openo.sdno.framework.container.util.JsonUtil;
+import org.openo.sdno.overlayvpn.model.netmodel.vxlan.NeVtep;
+import org.openo.sdno.overlayvpn.result.ResultRsp;
+import org.openo.sdno.testframework.http.model.HttpRequest;
+import org.openo.sdno.testframework.http.model.HttpResponse;
+import org.openo.sdno.testframework.http.model.HttpRquestResponse;
+import org.openo.sdno.testframework.moco.responsehandler.MocoResponseHandler;
+import org.openo.sdno.testframework.replace.PathReplace;
+
+public class QueryVtepResponseHandler extends MocoResponseHandler {
+
+    @Override
+    public void processRequestandResponse(HttpRquestResponse httpObject) {
+
+        HttpRequest httpRequest = httpObject.getRequest();
+        HttpResponse httpResponse = httpObject.getResponse();
+
+        ResultRsp<NeVtep> resultRsp =
+                JsonUtil.fromJson(httpResponse.getData(), new TypeReference<ResultRsp<NeVtep>>() {});
+        resultRsp.getData().setNeId(PathReplace.getUuidFromUrl("neid",
+                "/rest/svc/sbiadp/controller/{controller}//v1/vxlan/device/{neid}/vtep", httpRequest.getUri()));
+
+        httpResponse.setData(JsonUtil.toJson(resultRsp));
+    }
+}
