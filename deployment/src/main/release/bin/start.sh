e5b0d27d1e85a12f77bd8edeb4f91e328d372761
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2016-09-09 21:03:13 +0800

    Fix deploy project
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
    Change-Id: I3928e9aab0fe2df16c60b9b39b566afeb14d28cf

--- deployment/src/main/release/bin/start.sh
+++ deployment/src/main/release/bin/start.sh
@@ -14,30 +14,6 @@
 # limitations under the License.
 #*******************************************************************************
 #!/bin/bash
-
-if [ -z "$JAVA_HOME" ]
-then
-    echo "There is no JAVA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_HOME" ]
-then
-    echo "There is no CATALINA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_BASE" ]
-then
-    echo "There is no CATALINA_BASE"
-    exit 1
-fi
-
-LOG_DIR=$CATALINA_BASE/logs
-if [ ! -d "$LOG_DIR" ]; then
-  mkdir $LOG_DIR
-fi
-
-$CATALINA_HOME/bin/catalina.sh start
-
-
+cd ..
+export CATALINA_BASE=$(cd `dirname $0`; pwd)
+catalina.sh start
